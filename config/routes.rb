2ae8f8f74a98ff91047a1a7a5b9c8c334805ff52
ElvisDot <elvis.doettinger@gmail.com>
2018-11-12 16:36:53 +0100

    Add controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  resources :ingredients, only: :index
+  resources :recipes, only: [:index, :show]
   devise_for :users
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'ingredients#index'
 end
