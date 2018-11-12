4c5ea8e5dc1c0768b85c25aead6ee3518e41094c
ElvisDot <elvis.doettinger@gmail.com>
2018-11-12 16:48:31 +0100

    Add test user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  before_action :self_authenticate_user
+
+  def self_authenticate_user
+    @user = User.find_by_email('test@test.com')
+    sign_in(@user)
+  end
+
+  # def self_authenticate_user
+  #   if persistence_required?
+  #     authenticate_user!
+  #   elsif user == User.find_by_cookie_secret(get_cookie_secret)
+  #     sign_in(user)
+  #   else
+  #     cookie_secret = generate_cookie_secret
+  #     user = User.create!(cookie_secret: cookie_secret)
+  #     set_cookie_secret(cookie_secret)
+  #     sign_in(user)
+  #   end
+  # end
 end
