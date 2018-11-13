cbdb0117c1ecc776bd07e7d864a3dc32e38d4a7f
ElvisDot <elvis.doettinger@gmail.com>
2018-11-13 16:27:37 +0100

    Add ajax autocomplete

--- app/javascript/packs/ing-auto.js
+++ app/javascript/packs/ing-auto.js
@@ -1,23 +1,11 @@
 // variables
 const selectedListDom = document.getElementById("selected");
 const input = document.querySelector('input');
-const ingredients = ['potato', 'cucumber', 'garlic', 'chicken', 'cheese', 'sweet potato'];
 let items_to_show = [];
 const autocomplete_results = document.getElementById("autocomplete-results");
 const autocompleteInput = document.getElementById("autocomplete-input");
 
 // functions
-function autocomplete(val) {
-  const people_return = [];
-
-  for (let i = 0; i < ingredients.length; i++) {
-    if (val === ingredients[i].slice(0, val.length)) {
-      people_return.push(ingredients[i]);
-    }
-  }
-  return people_return;
-}
-
 function selectItem(item) {
   const list = document.createElement("li");
   list.innerText = item;
@@ -42,21 +30,19 @@ function addDropDownListeners() {
 }
 
 // events
-input.onkeyup = function(e) {
-  const input_val = this.value;
+autocompleteInput.addEventListener("keyup", (event) => {
+  // console.log("input: " + autocompleteInput.value);
+  fetch(`http://localhost:3000/ingredients.json?query=${autocompleteInput.value}`)
+    .then(response => response.json())
+    .then((data) => {
+      // console.log(data)
+      autocomplete_results.innerHTML = '';
+      data.forEach((ing) => {
+        console.log("ingredient: "+ ing);
+        autocomplete_results.innerHTML += '<li id="item-id">' + ing + '</li>';
+      });
+      addDropDownListeners();
+      autocomplete_results.style.display = 'block';
+    });
+});
 
-  if (input_val.length > 0) {
-    autocomplete_results.innerHTML = '';
-    items_to_show = autocomplete(input_val);
-
-    for (let i = 0; i < items_to_show.length; i++) {
-      autocomplete_results.innerHTML += `<li id="item-id">` + items_to_show[i] + '</li>';
-    }
-    addDropDownListeners();
-
-    autocomplete_results.style.display = 'block';
-  } else {
-    items_to_show = [];
-    autocomplete_results.innerHTML = '';
-  }
-}
