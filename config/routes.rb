7437141191121e9f2377d025c0224e11953a9856
ElvisDot <elvis.doettinger@gmail.com>
2018-11-14 12:05:47 +0100

    Change root route back to search

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :ingredients, only: :index
   resources :recipes, only: [:index, :show]
   devise_for :users
-  root to: 'pages#home'
+  root to: 'ingredients#index'
 end
