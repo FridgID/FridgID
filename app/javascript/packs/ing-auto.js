44109ce7afed1a2dc472a0e3a19ba9cc786aa3d9
ElvisDot <elvis.doettinger@gmail.com>
2018-11-14 10:54:07 +0100

    Don't double ingredients and create button

--- app/javascript/packs/ing-auto.js
+++ app/javascript/packs/ing-auto.js
@@ -4,6 +4,8 @@ const input = document.querySelector('input');
 let items_to_show = [];
 const autocomplete_results = document.getElementById("autocomplete-results");
 const autocompleteInput = document.getElementById("autocomplete-input");
+const submitDiv = document.getElementById("submit-link");
+const getButton = document.getElementById("get-recipes-btn");
 
 // functions
 function selectItem(item) {
@@ -21,7 +23,6 @@ function addDropDownListeners() {
   allItems.forEach((item) => {
     item.addEventListener("click", e => {
       const item = e.target.innerText;
-      // console.log("clicked list event: ", item);
       selectItem(item);
       autocomplete_results.innerHTML = "";
       autocompleteInput.value = '';
@@ -29,20 +30,40 @@ function addDropDownListeners() {
   });
 }
 
+function getSelectedItems() {
+  const selectedItems = [];
+  selectedListDom.childNodes.forEach((child) => {
+    selectedItems.push(child.innerHTML);
+  });
+  return [...new Set(selectedItems)]; // unique even if they shouldn't be doubled anyways
+}
+
+// Unused export to global replaced by eventlistener
+// window.getRecipes = getRecipes;
+
 // events
+getButton.addEventListener("click", (event) => {
+  window.location.href = `recipes/?i=${getSelectedItems().join('-')}`;
+});
+
 autocompleteInput.addEventListener("keyup", (event) => {
   // console.log("input: " + autocompleteInput.value);
   fetch(`/ingredients.json?query=${autocompleteInput.value}`)
     .then(response => response.json())
     .then((data) => {
-      // console.log(data)
       autocomplete_results.innerHTML = '';
+      const selectedItems = getSelectedItems();
       data.forEach((ing) => {
-        console.log("ingredient: "+ ing);
-        autocomplete_results.innerHTML += '<li id="item-id">' + ing + '</li>';
+        if (selectedItems.includes(ing)) {
+          // console.log("[+] ing '" + ing + "' is selcted already");
+        } else {
+          // console.log("[-] ing '" + ing + "' is not selected yet")
+          autocomplete_results.innerHTML += '<li id="item-id">' + ing + '</li>';
+        }
       });
       addDropDownListeners();
       autocomplete_results.style.display = 'block';
+      // console.log("selected stuff: " + getSelectedItems());
     });
 });
 
