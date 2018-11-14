65bde94f305513e59568b3a9e2d8ef968c56925d
ElvisDot <elvis.doettinger@gmail.com>
2018-11-14 16:21:19 +0100

    Basic recipe filter by ingredients
    The get recipe button on the ingredients page now leads to a page
    that only holds recipes that include the selected ingredients.
    
    And the ingredients api performance was also optimized

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,7 +1,6 @@
 class IngredientsController < ApplicationController
   def index
-    @ingredients = Ingredient.where("name LIKE :query", query: "%#{params[:query]}%").select(:name).limit(10)
-    @ingredients = @ingredients.map(&:name)
+    @ingredients = Ingredient.where("name LIKE :query", query: "%#{params[:query]}%").limit(10).pluck(:name)
 
     respond_to do |format|
       format.html # <-- will render `app/views/ingredients/index.html.erb`
