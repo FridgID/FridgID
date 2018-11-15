e1710f827655ded0588534cec1a0090448ccb969
ElvisDot <elvis.doettinger@gmail.com>
2018-11-15 17:45:41 +0100

    Add seasonal? method for ingredients

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,21 @@
 class Ingredient < ApplicationRecord
+  # use me :)
+  def seasonal?
+    from = Date::MONTHNAMES.index(from_month)
+    to = Date::MONTHNAMES.index(to_month)
+    return Ingredient.season_month?(from, to, Date.today.month)
+  end
+
+  # only a helper
+  def self.season_month?(from, to, now)
+    return from == now if from == to
+    return (from..to).cover?(now) if from < to
+
+    # from > to
+    return (from..(to + 12)).cover?(now + (now > from ? 0 : 12))
+  end
+
+  # active record
   has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredients
 
@@ -6,35 +23,35 @@ class Ingredient < ApplicationRecord
   validates :from_month, inclusion: {
     in: [
       '', # empty str for seasonless ingr like salt
-      'january',
-      'february',
-      'march',
-      'april',
-      'may',
-      'june',
-      'july',
-      'august',
-      'september',
-      'october',
-      'november',
-      'december'
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
     ]
   }
   validates :to_month, inclusion: {
     in: [
       '', # empty str for seasonless ingr like salt
-      'january',
-      'february',
-      'march',
-      'april',
-      'may',
-      'june',
-      'july',
-      'august',
-      'september',
-      'october',
-      'november',
-      'december'
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
     ]
   }
 end
--- test/models/ingredient_test.rb
+++ test/models/ingredient_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class IngredientTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # def season_month?(f, t, now)
+  i = Ingredient
+
+  # simple
+  test "season_month? [6-(8)-10]" do
+    assert i.season_month?(6, 10, 8) == true
+  end
+
+  test "season_month? [6-10] (11)" do
+    assert i.season_month?(6, 10, 11) == false
+  end
+
+  # overlapping
+  test "season_month? [10-(11)-1]" do
+    assert i.season_month?(10, 1, 11) == true
+  end
+
+  test "season_month? (9) [10-1]" do
+    assert i.season_month?(10, 1, 9) == false
+  end
+
+  test "season_month? [10-1] (2)" do
+    assert i.season_month?(10, 1, 2) == false
+  end
+
+  test "season_month? [10-(2)-5]" do
+    assert i.season_month?(10, 5, 2) == true
+  end
 end
