cf426a1f92551a765630e6842e2bdc0cf33627df
ElvisDot <elvis.doettinger@gmail.com>
2018-11-16 12:41:17 +0100

    Fix recipe stats

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,8 +6,9 @@ class Recipe < ApplicationRecord
     title.truncate(32)
   end
 
+  # returns number of given ingredients that match with this recipe
   def ingredients_match(selected_ingrs)
-    ingredients.map(&:name) & selected_ingrs
+    (ingredients.map(&:name) & selected_ingrs).count
     # com = ingredients.map(&:name) & selected_ingrs
     # puts "-----------------"
     # puts "ingrs: #{ingredients.map(&:name)}"
@@ -15,11 +16,16 @@ class Recipe < ApplicationRecord
     # puts "common: #{com}"
   end
 
+  # returns seasonal score in percent
   def seasonal_score
     score = 0
     ingredients.each do |ingr|
       score += ingr.seasonal
     end
-    (ingredients.count * 2 / score) * 10
+    max = ingredients.count * 2
+    (score * 100 / max)
+    # puts "--------------"
+    # puts "recipe: #{title}"
+    # puts "score #{score} / #{max}"
   end
 end
