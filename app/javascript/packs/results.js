108e747f8c89882b04c1192438ca4bce9a30fe13
Karin Lilliehöök <karin@lilliehooks.se>
2018-11-16 15:20:22 +0100

    show page done for demo

--- app/javascript/packs/results.js
+++ app/javascript/packs/results.js
@@ -1,15 +1,21 @@
 const btnMethod = document.getElementById('btn-method');
 const btnIngrs = document.getElementById('btn-ingrs');
+const pageMethodHead = document.getElementById('page-method-head');
 const pageMethod = document.getElementById('page-method');
+const pageIngrsHead = document.getElementById('page-ingredients-head');
 const pageIngrs = document.getElementById('page-ingredients');
 
 // events
 btnMethod.addEventListener('click', (e) => {
+  pageMethodHead.style.display = "block";
+  pageIngrsHead.style.display = "none";
   pageMethod.style.display = "block";
   pageIngrs.style.display = "none";
 });
 
 btnIngrs.addEventListener('click', (e) => {
+  pageMethodHead.style.display = "none";
+  pageIngrsHead.style.display = "block";
   pageMethod.style.display = "none";
   pageIngrs.style.display = "block";
 });
