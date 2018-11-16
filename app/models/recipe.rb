3c0502121344021358242c63eb96ba1b5ac1bbd0
ElvisDot <elvis.doettinger@gmail.com>
2018-11-16 11:50:03 +0100

    Backend for how many ingrs match

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,8 +2,16 @@ class Recipe < ApplicationRecord
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
 
-
   def shortened_title
     title.truncate(32)
+  end
+
+  def ingredients_match(selected_ingrs)
+    ingredients.map(&:name) & selected_ingrs
+    # com = ingredients.map(&:name) & selected_ingrs
+    # puts "-----------------"
+    # puts "ingrs: #{ingredients.map(&:name)}"
+    # puts "selected: #{selected_ingrs}"
+    # puts "common: #{com}"
   end
 end
