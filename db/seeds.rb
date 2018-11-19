da99350953c0419c3467f7167c6e6dca4eaf5a0f
ElvisDot <elvis.doettinger@gmail.com>
2018-11-19 11:54:45 +0100

    Json parser reads recipes

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,14 @@ Ingredient.destroy_all
 puts "create test user..."
 User.create!(email: 'test@test.com', password: 'password')
 
-# require_relative '../lib/seed_json.rb'
-# puts "loading json seeds..."
-# json_ingredients
+require_relative '../lib/seed_json.rb'
+puts "loading json seeds..."
+json_ingredients
+json_recipes
+exit
 # require_relative '../lib/parse_raw.rb'
 # puts "parsing raw..."
 # parse_raw
-# exit
 
 puts "create ingredients..."
 pkn = Ingredient.create!(name: 'pumpkin', from_month: 'August', to_month: 'December')
--- lib/seed_json.rb
+++ lib/seed_json.rb
@@ -1,7 +1,39 @@
+# rubocop: disable all
+###############
+# ingredients #
+###############
 def json_ingredients
   data = JSON.parse(File.read(Rails.root.to_s + '/lib/json/ingredients.json'))
   data.each do |ing|
     puts "creating #{ing['name']} -> #{ing['name'].singularize}"
     Ingredient.create!(name: ing['name'].singularize, from_month: ing['from_month'], to_month: ing['to_month'])
+  end
+end
+
+###########
+# recipes #
+###########
+# helper
+def json_to_recipe(r)
+  Recipe.create!(
+    title: r['title'],
+    description: r['description'],
+    method: r['method'],
+    photo: r['photo']
+  )
+end
+
+# main
+def json_recipes
+  data = JSON.parse(File.read(Rails.root.to_s + '/lib/json/recipes.json'))
+  data.each do |r|
+    puts "Recipe '#{r['title']}'"
+    r_db = json_to_recipe(r)
+    r['ingredients'].each do |i|
+      # puts "+ #{i}"
+      i_db = Ingredient.find_by(name: i)
+      RecipeIngredient.create!(recipe_id: r_db.id, ingredient_id: i_db.id)
+    end
+    puts "ingredients: #{r_db.ingredients.count}"
   end
 end
