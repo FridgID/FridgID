c2e4858b6902031a79affc5baf7b725defee7f48
ElvisDot <elvis.doettinger@gmail.com>
2018-11-21 10:53:03 +0100

    Recipe scraper is working

--- lib/parse_recipe.rb
+++ lib/parse_recipe.rb
@@ -1,15 +1,36 @@
 require 'json'
+require 'rainbow'
 require 'nokogiri'
 require 'pry-byebug'
+require 'rubygems'
+require 'active_support/inflector'
 
-puts "[+] ----------- [start] ------------ [+]"
-puts "[*] loading db ingredients..."
-db_ingrs = []
+def pok(str) # print ok
+  puts "[" + Rainbow("+").green + "] " + str
+end
+
+def pwr(str) # print warning
+  puts "[" + Rainbow("!").orange + "] " + str
+end
+
+def per(str) # print error
+  puts "[" + Rainbow("-").red + "] " + str
+end
+
+def pif(str) # print info
+  puts "[" + Rainbow("*").green + "] " + str
+end
+
+pok "----------- [start] ------------"
+pif "loading db ingredients..."
+$db_ingrs = []
 data = JSON.parse(File.read('json/ingredients.json'))
 data.each do |d|
-  db_ingrs.push(d['name'])
+  $db_ingrs.push(d['name'])
 end
-puts "[+] loaded #{db_ingrs.count} ingredients!"
+pok "create ingredient hash..."
+$db_hash = $db_ingrs.map {|i| [i, i]}.to_h
+pok "loaded #{$db_ingrs.count} ingredients!"
 
 path = ARGV[0] ? ARGV[0] : 'html/001.html'
 
@@ -19,7 +40,7 @@ doc = Nokogiri::HTML(html_content)
 title = "not found"
 desc = "not found"
 method = "not found"
-metrics = []
+$metrics = []
 
 doc.search('.recipe-intro').each do |element|
   # binding.pry
@@ -34,7 +55,7 @@ end
 e = doc.search('.recipe-container picture img')
 img_url = e.first.attributes['src'].value[2..-2]
 if e == []
-  puts "error"
+  per "error image not found"
   exit
 end
 
@@ -45,12 +66,53 @@ doc.search('.method-p').each do |element|
       # puts li.children[0].text
       method.push(li.children[0].text)
     else
-      puts "[*] skipping cookie banner"
+      pif "skipping cookie banner"
       # puts "li.children[0] doesnt exist"
       # puts "probably cookie banner whatevery"
       # binding.pry
     end
   end
+end
+
+def find_ingredient(ingredient_metric, ingredients)
+  m = ingredient_metric #.split(' ')
+  if m.count == 1
+    pwr "WARNING only one word in metrics"
+  end
+
+  trigrams = m.each_slice(3).to_a + m[1..-1].each_slice(3).to_a + m[2..-1].each_slice(3).to_a
+  digrams = m.each_slice(2).to_a + m[1..-1].each_slice(2).to_a
+  grams = trigrams + digrams
+  grams.each do |gram|
+    if ingredient = ingredients[gram.join(" ")]
+      return ingredient
+    end
+   end
+   nil # could not find ingredient
+end
+
+def tridi_grams(ingrs, raw_ing)
+  # TRIGRAMS AND DIGRAMS
+  tridi = find_ingredient(ingrs, $db_hash)
+  if !tridi
+    pif "No tri or digrams"
+  else
+    pok "tridi found '#{tridi}'"
+    $metrics.push(tridi, raw_ing)
+    return true
+  end
+  false
+end
+
+def single_word(ingrs, raw_ing)
+  col = ingrs & $db_ingrs
+  return false if col == []
+  pok "collision found: #{col}"
+  if col.count > 1
+    pwr "WARNING! collision is bigger than 1"
+  end
+  $metrics.push(col[0], raw_ing)
+  true
 end
 
 doc.search('.recipe-ingredients .ingred-list').each do |element|
@@ -59,28 +121,47 @@ doc.search('.recipe-ingredients .ingred-list').each do |element|
   element.children.each_with_index do |child, i|
     if i % 2 != 0
       raw_ing = child.children.text
-      col = raw_ing.split & db_ingrs
-      if col != []
-        puts "[+] collision found: #{col}"
-        if col.count > 1
-          puts "[!] WARNING! collision is bigger than 1"
-        end
-      else
-        puts "[-] ingredient not found"
+      if raw_ing == raw_ing.upcase
+        pwr "skip heading #{raw_ing}"
+        next
       end
-      metrics.push(col[0], raw_ing)
+      # ingrs = raw_ing.split.map(&:singularize)
+      ingrs = raw_ing.split.map do |i|
+        if i[-1] == 'a'
+          pif "PASTUM!!!!!!!!!!!!!!"
+          i
+        else
+          i.singularize
+        end
+      end
+      # TRIGRAMS AND DIGRAMS
+      next if tridi_grams(ingrs, raw_ing)
+
+      # SINGLE WORDS
+      next if single_word(ingrs, raw_ing)
+
+      # GSUB FALLBACK
+      pif "fallback to gsub [,()]"
+      ingrs = ingrs.map{ |a| a.gsub(/[,\(\)]/, '')}
+      next if tridi_grams(ingrs, raw_ing)
+      next if single_word(ingrs, raw_ing)
+
+      per "ingredient not found"
+      puts "ingrs: #{ingrs}"
+      # puts raw_ing.split.map(&:singularize)
+      $metrics.push("ERROR", "NOT IN DB")
     end
   end
 end
 
-puts "[+] finished '#{title}' !"
+pok "finished '#{title}' !"
 #puts "------------------------"
 #puts "title: #{title}"
 #puts "desc: #{desc}"
 #puts "------------------------"
 #puts "method: #{method}"
 #puts "------------------------"
-#puts "ingrs: #{metrics}"
+#puts "ingrs: #{$metrics}"
 
 
 rec_hash = {
@@ -88,7 +169,7 @@ rec_hash = {
   description: desc,
   method: method.join(' '),
   photo: img_url,
-  metrics: metrics
+  metrics: $metrics
 }
 
 File.open("json/scrape_recipes.json","a") do |f|
